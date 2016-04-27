1eb27f89273b565a70b1eb987b376b141f1a6f19
hypoyao <hypoyao@qq.com>
2016-04-27 10:52:10 +0800

    Update NSObjectSafe.m
    need a assert when objects or keys contain nil

--- NSObjectSafe/NSObjectSafe.m
+++ NSObjectSafe/NSObjectSafe.m
@@ -459,6 +459,8 @@ void SFLog(const char* file, const char* func, int line, NSString* fmt, ...)
             ks[index] = keys[i];
             objs[index] = objects[i];
             ++index;
+        } else {
+            SFAssert(NO, @"NSDictionary invalid args hookDictionaryWithObject:[%@] forKey:[%@]", objects[i], keys[i]);
         }
     }
     return [self hookDictionaryWithObjects:objs forKeys:ks count:index];
